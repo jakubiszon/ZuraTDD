6f6ca524693257c8a76753d524c69b97109bcbe9
jakubiszon <jakubzurawski@gmail.com>
2026-06-05 01:50:05 +0200

    remove unused code

--- ExampleProject/ContentPublishedEventHandler.cs
+++ ExampleProject/ContentPublishedEventHandler.cs
@@ -37,9 +37,4 @@ internal class ContentPublishedEventHandler : IContentPublishedEventHandler
 			await emailSender.SendEmail(customer.Email, subject, body);
 		}
 	}
-
-	public void RollbackTransaction()
-	{
-		throw new NotImplementedException();
-	}
 }

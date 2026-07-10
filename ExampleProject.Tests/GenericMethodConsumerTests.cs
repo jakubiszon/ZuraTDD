fb821e9dc24abd69d09c27e2467cc1818595bf58
jakubiszon <jakubzurawski@gmail.com>
2026-07-10 14:32:57 +0200

    tests - add basic tests using GenericArgument classes

--- ExampleProject.Tests/GenericMethodConsumerTests.cs
+++ ExampleProject.Tests/GenericMethodConsumerTests.cs
@@ -17,14 +17,26 @@ public partial class GenericMethodConsumerTests
 		Expect.GenericMethods
 			.DoSomething<string>()
 			.WasNotCalled(),
+
+		// make sure DoSomething<T1,T2,T3> was not calledregardless of the generic type arguments
+		Expect.GenericMethodsWithWhere
+			.DoSomething<GenericArgument.AnyType, GenericArgument.AnyType, GenericArgument.AnyType>()
+			.WasNotCalled(),
 	];
 
 	[ZuraTest<GenericMethodConsumerTestCase>("Calling generic method with where.")]
 	private ITestPart[] CallingGenericMethodWithWhere => [
 		Receives.SomeMethod(2),
 
+		// make sure DoSomething was called with the expected generic type arguments
 		Expect.GenericMethodsWithWhere
 			.DoSomething<IEnumerable<int>, int, object>()
 			.WasCalled(),
+
+		// make sure DoSomething was called just once regardless of the generic type arguments
+		// in other words - no calls other than the one we expected
+		Expect.GenericMethodsWithWhere
+			.DoSomething<GenericArgument.AnyType, GenericArgument.AnyType, GenericArgument.AnyType>()
+			.WasCalled(1),
 	];
 }
